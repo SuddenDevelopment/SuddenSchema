6316c1a25ffaee66129064caebd2fb3946ac4650
Anthony Aragues <anthonyaragues@aaragues.local>
2016-07-26 18:36:46 -0400

    added country code detection

--- SuddenSchema.js
+++ SuddenSchema.js
@@ -89,6 +89,7 @@ var SuddenSchema = function(objConfig){
 				}
 			}else{
 				//no value found for this object at specified key of v 
+				//console.log('couldnt find: ', v);
 			}
 		});		
 		return objSchema;
@@ -98,7 +99,7 @@ var SuddenSchema = function(objConfig){
 		objVal.typ='object';
 		//When this happens it means the object wasnt mapped out for properties to be added to keys yet OR it's an array
 		if(val.constructor===Array){
-			objVal.dataTypes=['array'];
+			
 		}else{
 			//we should never get here because every object has deepkeys run on it and added to the keys in the schema
 		}
@@ -185,6 +186,9 @@ var SuddenSchema = function(objConfig){
 
 		return objVal;
 	};
+	endVal.array=function(objVal){
+		return objVal;
+	}
 	endVal.boolean=function(objVal){
 
 		return objVal;
@@ -239,6 +243,7 @@ var SuddenSchema = function(objConfig){
       objTests.md5= /^[a-f0-9]{32}$/;
       objTests.sha1= /^[0-9a-f]{40}$/;
       objTests.sha256= /^[0-9a-f]{64}$/;
+      objTests.country_code = /^(AF|AX|AL|DZ|AS|AD|AO|AI|AQ|AG|AR|AM|AW|AU|AT|AZ|BS|BH|BD|BB|BY|BE|BZ|BJ|BM|BT|BO|BQ|BA|BW|BV|BR|IO|BN|BG|BF|BI|KH|CM|CA|CV|KY|CF|TD|CL|CN|CX|CC|CO|KM|CG|CD|CK|CR|CI|HR|CU|CW|CY|CZ|DK|DJ|DM|DO|EC|EG|SV|GQ|ER|EE|ET|FK|FO|FJ|FI|FR|GF|PF|TF|GA|GM|GE|DE|GH|GI|GR|GL|GD|GP|GU|GT|GG|GN|GW|GY|HT|HM|VA|HN|HK|HU|IS|IN|ID|IR|IQ|IE|IM|IL|IT|JM|JP|JE|JO|KZ|KE|KI|KP|KR|KW|KG|LA|LV|LB|LS|LR|LY|LI|LT|LU|MO|MK|MG|MW|MY|MV|ML|MT|MH|MQ|MR|MU|YT|MX|FM|MD|MC|MN|ME|MS|MA|MZ|MM|NA|NR|NP|NL|NC|NZ|NI|NE|NG|NU|NF|MP|NO|OM|PK|PW|PS|PA|PG|PY|PE|PH|PN|PL|PT|PR|QA|RE|RO|RU|RW|BL|SH|KN|LC|MF|PM|VC|WS|SM|ST|SA|SN|RS|SC|SL|SG|SX|SK|SI|SB|SO|ZA|GS|SS|ES|LK|SD|SR|SJ|SZ|SE|CH|SY|TW|TJ|TZ|TH|TL|TG|TK|TO|TT|TN|TR|TM|TC|TV|UG|UA|AE|GB|US|UM|UY|UZ|VU|VE|VN|VG|VI|WF|EH|YE|ZM|ZW)$/;
       //run each of the tests for each of the values, return the types found
       _.for(arrHaystack,function(v,k){
       	_.forOwn(objTests,function(vv,kk){
